0d4e6c64dba3fc74e5b9c54b8b8d5e1929c93b41
Krzysztof Guz <krzysztof.guz@me.com>
2022-04-15 13:48:12 +0100

    add section to AllDayEvent

--- Example/JZCalendarWeekViewExample/Source/LongPressViews/AllDayEvent.swift
+++ Example/JZCalendarWeekViewExample/Source/LongPressViews/AllDayEvent.swift
@@ -13,15 +13,15 @@ class AllDayEvent: JZAllDayEvent {
     var location: String
     var title: String
 
-    init(id: String, title: String, startDate: Date, endDate: Date, location: String, isAllDay: Bool) {
+    init(id: String, title: String, startDate: Date, endDate: Date, location: String, isAllDay: Bool, section: Int) {
         self.location = location
         self.title = title
 
         // If you want to have you custom uid, you can set the parent class's id with your uid or UUID().uuidString (In this case, we just use the base class id)
-        super.init(id: id, startDate: startDate, endDate: endDate, isAllDay: isAllDay)
+        super.init(id: id, startDate: startDate, endDate: endDate, isAllDay: isAllDay, section: section)
     }
 
     override func copy(with zone: NSZone?) -> Any {
-        return AllDayEvent(id: id, title: title, startDate: startDate, endDate: endDate, location: location, isAllDay: isAllDay)
+        return AllDayEvent(id: id, title: title, startDate: startDate, endDate: endDate, location: location, isAllDay: isAllDay, section: section)
     }
 }
