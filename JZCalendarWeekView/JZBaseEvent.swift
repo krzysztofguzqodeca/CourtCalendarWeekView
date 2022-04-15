44bf1c172ef46ffc4595bcc5a44dc18b4fc8c86d
Krzysztof Guz <krzysztof.guz@me.com>
2022-04-15 13:43:52 +0100

    add section to JZBaseEvent

--- JZCalendarWeekView/JZBaseEvent.swift
+++ JZCalendarWeekView/JZBaseEvent.swift
@@ -12,19 +12,20 @@ open class JZBaseEvent: NSObject, NSCopying {
 
     /// Unique id for each event to identify an event, especially for cross-day events
     public var id: String
-
     public var startDate: Date
     public var endDate: Date
+    public var section: Int
 
     // If a event crosses two days, it should be devided into two events but with different intraStartDate and intraEndDate
     // eg. startDate = 2018.03.29 14:00 endDate = 2018.03.30 03:00, then two events should be generated: 1. 0329 14:00 - 23:59(IntraEnd) 2. 0330 00:00(IntraStart) - 03:00
     public var intraStartDate: Date
     public var intraEndDate: Date
 
-    public init(id: String, startDate: Date, endDate: Date) {
+    public init(id: String, startDate: Date, endDate: Date, section: Int) {
         self.id = id
         self.startDate = startDate
         self.endDate = endDate
+        self.section = section
         self.intraStartDate = startDate
         self.intraEndDate = endDate
     }
@@ -32,6 +33,6 @@ open class JZBaseEvent: NSObject, NSCopying {
     // Must be overridden
     // Shadow copy is enough for JZWeekViewHelper to create multiple events for cross-day events
     open func copy(with zone: NSZone? = nil) -> Any {
-        return JZBaseEvent(id: id, startDate: startDate, endDate: endDate)
+        return JZBaseEvent(id: id, startDate: startDate, endDate: endDate, section: section)
     }
 }
